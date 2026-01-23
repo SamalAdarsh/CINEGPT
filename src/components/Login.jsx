5b4e46528e93b4f224965125a5bcaf59e4a77e9c
Adarsh <codewithadarsh007@gmail.com>
2026-01-23 16:44:47 +0530

    Sign Up Page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+
+    const [isSignInForm, setIsSignInForm] = useState(true);
+
+   const toggleSignIn = ()=>{
+  setIsSignInForm(!isSignInForm);
+
+   } 
   return (
     <div>
       <Header />
@@ -12,11 +19,13 @@ const Login = () => {
         />
       </div>
       <form className="absolute bg-black w-3/12 my-36 mx-auto right-0 left-0 text-white  p-12 opacity-90 ">
-        <h1 className="font-bold text-3xl">Sign In</h1>
+        <h1 className="font-bold text-3xl">{isSignInForm? "Sign In" : "Sign Up"}</h1>
+        {!isSignInForm &&  <input type="text" placeholder="Full Name" className="p-4 my-4 w-full bg-gray-700"/>}
         <input type="text" placeholder="Email Address" className="p-4 my-4 w-full bg-gray-700"/>
         <input type="password" placeholder="Password" className="p-4 my-4 w-full bg-gray-700"/>
-        <button className="p-4 my-6 w-full bg-red-700 ">Sign In</button>
-        <p className="py-4">New to NetFlix? Sign Up</p>
+         {!isSignInForm &&  <input type="number" placeholder="Phone Number" className="p-4 my-4 w-full bg-gray-700"/>}
+        <button className="p-4 my-6 w-full bg-red-700 ">{isSignInForm? "Sign In" : "Sign Up"}</button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignIn} >{isSignInForm? "New to NetFlix? Sign Up" : "Already Signedup? Sign In"}</p>
       </form>
     </div>
   );
