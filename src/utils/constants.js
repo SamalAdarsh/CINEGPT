71f1923913bde70e0f55bb42ee74125f27aeb6af
Adarsh <codewithadarsh007@gmail.com>
2026-02-01 17:46:59 +0530

    Securing API Key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,17 +1,10 @@
+
 export const LOGO_URL =
   "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2026-01-09/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
 export const USER_AVATAR =
   "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg";
 
-export const API_OPTIONS = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDg2MzYwMjkyMjQ4MmIzNzkyYmQxMGNkNzZiYWViNiIsIm5iZiI6MTc2OTM1MzA5MC42NTkwMDAyLCJzdWIiOiI2OTc2MmY4MjY5Y2M1MWJlNmRhZTM0YzMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.-NnY7DrgCaaSLsXolxJOzvrJpfnLnSUW1jAlqlC6-XE",
-  },
-};
 
 export const POSTER_CDN_URL = "https://image.tmdb.org/t/p/w500";
 
@@ -24,6 +17,13 @@ export const SUPPORTED_LANGUAGE = [
   { identifier: "spanish", name: "Spanish" },
 ];
 
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer " + import.meta.env.VITE_TMDB_API_KEY,
+  },
+};
 
-export const OPENAI_API_KEY = "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";
-
+export const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
